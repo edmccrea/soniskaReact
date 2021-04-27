7ac2747edc2fb6b098ff8dc242cc64975ad881d8
edmccrea <ed-mccrea@hotmail.co.uk>
2021-04-27 21:29:16 +0200

    updated sign up form

--- client/src/compnents/auth/Register.js
+++ client/src/compnents/auth/Register.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Register.css';
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Passwords do not match');
+    } else {
+      console.log('Success');
+    }
+  };
   return (
     <div class='user-body signup-body'>
       <div class='user-container'>
         <div class='user-main-contents'>
           <h1 class='user-title'>Sign Up</h1>
-          <form action='/signup' method='POST' class='user-form'>
+          <form onSubmit={(e) => onSubmit(e)} class='user-form'>
             <input
               class='user-form-input'
               type='text'
-              placeholder='Username'
-              name='username'
+              placeholder='Name'
+              name='name'
+              value={name}
+              onChange={(e) => onChange(e)}
               required
             />
             <input
@@ -21,6 +43,8 @@ const Register = () => {
               type='email'
               placeholder='Email'
               name='email'
+              onChange={(e) => onChange(e)}
+              value={email}
               required
             />
             <input
@@ -28,6 +52,19 @@ const Register = () => {
               type='password'
               placeholder='Password'
               name='password'
+              minLength='6'
+              value={password}
+              onChange={(e) => onChange(e)}
+              required
+            />
+            <input
+              class='user-form-input'
+              type='password'
+              placeholder='Retype Password'
+              name='password2'
+              minLength='6'
+              value={password2}
+              onChange={(e) => onChange(e)}
               required
             />
             <div class='t-c'>
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -9,7 +9,7 @@ const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
 
 //@route  GET api/auth
-//@desc   Get Auth User
+//@desc   Get Authorized User
 //@access Public
 router.get('/', auth, async (req, res) => {
   try {
